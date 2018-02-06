48fa3ec5e9beb044dceb16b00ec2232532b3f154
Jan Weidner <jw3126@gmail.com>
2018-02-06 10:29:38 +0100

    behaviour on fail etc.

--- src/core.jl
+++ src/core.jl
@@ -2,10 +2,49 @@ export RegressionTester
 
 struct RegressionTester
     cache::Associative{String, Any}
+    read_cache_action::Function # :: RegressionTester -> key -> computed -> (cached, computed)
+    result_action::Function # :: RegessionTester -> key -> cached -> computed -> result -> Any
+end
+
+function read_cache_action(r::RegressionTester, args...;kw...)
+    r.read_cache_action(r, args...; kw...)
+end
+function result_action(r::RegressionTester, args...; kw...)
+    r.result_action(r, args...;kw...)
+end
+
+function prompt_missing_keys(r, key, computed)
+    if key ∈ keys(r.cache)
+        cached = r.cache[key]
+    elseif readbool("$key is missing. Should $computed be inserted?")
+        cached = get!(r.cache, key, computed)
+    else
+        throw(KeyError(key))
+    end
+    cached, computed
+end
+
+pass_result(r,key,cached,computed,result) = result
+
+function prompt_replace(r,key,cached,computed,result)
+    if !result
+        println("Regression test failure:")
+        println("Key: $key")
+        println("Cached: $cached")
+        println("Computed: $computed")
+        replace = readbool("Should the current cached value be replaced, with the computed value?")
+        if replace
+            r.cache[key] = computed
+            cached = computed
+            return true
+        end
+    end
+    result
 end
 
 function readbool(msg)
     println(msg)
+    println("provide true/false")
     try
         return parse(Bool, readline())
     catch e
@@ -14,18 +53,18 @@ function readbool(msg)
     end
 end
 
-function RegressionTester(path::AbstractString)
+function RegressionTester(path::AbstractString; kw...)
     cache = JLDBackedCache(path)
-    RegressionTester(cache)
+    RegressionTester(cache; kw...)
+end
+function RegressionTester(cache::Associative,
+                          read_cache_action=prompt_missing_keys,
+                          result_action=pass_result)
+    RegressionTester(cache, read_cache_action, result_action)
 end
 
 function (r::RegressionTester)(f, key, computed; opts...)
-    if key ∈ keys(r.cache)
-        cached = r.cache[key]
-    elseif readbool("$key is missing. Should $computed be inserted?")
-        cached = get!(r.cache, key, computed)
-    else
-        throw(KeyError(key))
-    end
-    f(cached, computed)
+    cached, computed = read_cache_action(r, key, computed)
+    result = f(cached, computed)
+    result_action(r, key, cached, computed, result)
 end
